3ab796641ee8501b04224cb92d68390ac0339bc7
Laura Goon <Laura.goon@ou.edu>
2021-02-23 17:54:46 -0600

    finished example

---
+++ Loops_For/Loops_For/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Loops_For
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Count all the way up to 100
+            for (int i = 1; i < 101; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = 3; i < 1001; i +=3)
+            {
+                //if (i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            for (int i = 10; i >= 0; i--)
+            {
+                if (i == 0)
+                {
+                    Console.WriteLine("Blast off");
+                }
+                else
+                {
+                 Console.WriteLine(i);
+                }
+            }
+
+
+        }
+    }
+}
