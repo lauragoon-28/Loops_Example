9bf272c1617d336a485454f22704c6dc785abb85
Laura Goon <Laura.goon@ou.edu>
2021-02-23 18:01:55 -0600

    finished example

---
+++ Loops_While/Loops_While/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Loops_While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your lucky # >>");
+            string answer = Console.ReadLine();
+            int luckyNumber;
+
+            while (int.TryParse(answer , out luckyNumber) == false)
+            {
+                Console.WriteLine("Sorry you entered an invalid integer. Please enter another #");
+                answer = Console.ReadLine();
+            }
+
+            Console.WriteLine("Yay you entered an integer");
+        }
+    }
+}
